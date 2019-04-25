89167ba778fbf9f489abcf455b26b040e20e8b50
giacomocherubini <giacomo.cherubini1@gmail.com>
2019-04-25 12:29:59 +0200

    fine

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-
+// ciclo for numeri da 1 a 100
 for(var i = 1; i<=100; i++) {
-    if ((i % 3 && i % 5) == 0) {
-    document.writeln('FizzBuzz' + '<br>')
+// se sono multipli di 3 e 5 scrivo FizzBuzz
+    if ((i % 3) == 0 && (i % 5) == 0) {
+    document.writeln('FizzBuzz' + '<br>');
+    // altrimenti se sono multipli di 3 scrivo Fizz
   } else if ((i % 3) == 0) {
-    document.writeln('Fizz' + '<br>')
+    document.writeln('Fizz' + '<br>');
+    // altrimenti se sono multipli di 5 scrivo Buzz
   } else if ((i % 5) == 0) {
-    document.writeln('Buzz' + '<br>')
+    document.writeln('Buzz' + '<br>');
+    // altrimenti scrivo numeri
   } else {
     document.writeln(i + '<br>');
   }
